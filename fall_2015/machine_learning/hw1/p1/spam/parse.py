ad6abebdfbcedde7ab57523aac9d000b9b3d5594
Luke Jaffe <lukejaffe1@gmail.com>
2015-09-17 18:19:48 -0400

    Fix/rename eval_node

--- fall_2015/machine_learning/hw1/p1/spam/parse.py
+++ fall_2015/machine_learning/hw1/p1/spam/parse.py
@@ -1,6 +1,16 @@
 from bitstring import BitArray
 from math import log
 from operator import itemgetter
+import time
+
+def timing(f):
+    def wrap(*args):
+        time1 = time.time()
+        ret = f(*args)
+        time2 = time.time()
+        print '%s function took %0.3f ms' % (f.func_name, (time2-time1)*1000.0)
+        return ret
+    return wrap
 
 feature_list = []
 
@@ -60,33 +70,115 @@ def entropy(p):
 def info_gain():
     pass
 
-A = data
-p_true = prob(A, Y, spam=True)
-p_false = 1.0 - p_true
-print entropy([p_true, p_false])
+@timing
+def build_tree1(fmat, feature_list, thresh):
+    B, C = '', ''
+    e_list = []
+    for f in [feature_list[0]]:
+        print f
+          
+        for t in thresh[f]:
+            #print t,
+            s = ''
+            for i in range(len(fmat[f])):
+                if fmat[f][i] < t:
+                    s += '1'
+                else:
+                    s += '0'
+            B = BitArray(bin=s)
+            #C = B.copy()
+            #C.invert()
 
-B, C = '', ''
-e_list = []
-for f in [feature_list[0]]:
-    print f
-      
-    for t in thresh[f]:
-        #print t,
-        s = ''
-        for i in range(len(fmat[f])):
-            if fmat[f][i] < t:
-                s += '1'
-            else:
-                s += '0'
+            p_true = prob(B, Y, spam=True)
+            p_false = 1.0 - p_true
+            e = entropy([p_true, p_false])
+            if e > 0.0:
+                e_list.append((f,t,e))
+
+    min_val = min(e[2] for e in e_list)
+    print "Min:",[e for e in e_list if e[2] == min_val]
+
+@timing
+def build_tree2(fmat, feature_list):
+    B, C = '', ''
+    e_list = []
+    for f in [feature_list[0]]:
+        print f
+
+        # Sort the relevant column and keep indices
+        pairs = [sorted(enumerate(fmat[f]), key=itemgetter(1))][0]
+
+        s = len(pairs)*'0'
         B = BitArray(bin=s)
-        #C = B.copy()
-        #C.invert()
+        i = 0
+        # Threshold emulator loop
+        while True:
+            B[pairs[i][0]] = 1
+            if pairs[i][1] < pairs[i+1][1]:
+                t = pairs[i+1][1]
+                # Evaluate entropy and yield result
+                p_true = prob(B, Y, spam=True)
+                p_false = 1.0 - p_true
+                e = entropy([p_true, p_false])
+                e_list.append((f,t,e))
+            i += 1
+            # Check end condition
+            if i >= len(pairs)-1:
+                break
 
-        p_true = prob(B, Y, spam=True)
-        p_false = 1.0 - p_true
-        e = entropy([p_true, p_false])
-        if e > 0.0:
-            e_list.append((f,t,e))
+    min_val = min(e[2] for e in e_list)
+    print "Min:",[e for e in e_list if e[2] == min_val]
 
-min_val = min(e[2] for e in e_list)
-print "Min:",[e for e in e_list if e[2] == min_val]
+@timing
+def eval_node(A, fmat, feature_list):
+    IG_list = []
+    A_count = float(A.count(True))
+    p_A = prob(A, Y, spam=True)
+    e_A = entropy([p_A, 1.0-p_A])
+    for f in feature_list:
+        # Sort the relevant column and keep indices
+        pairs = [sorted(enumerate(fmat[f]), key=itemgetter(1))][0]
+
+        s = len(pairs)*'0'
+        B = BitArray(bin=s)
+        C = A.copy()
+        i = 0
+        IG_prev = 0.0
+        # Threshold emulator loop
+        while True:
+            B[pairs[i][0]] = 1
+            C[pairs[i][0]] = 0
+            if pairs[i][1] < pairs[i+1][1]:
+                t = pairs[i+1][1]
+                # Calculate probability for child nodes
+                p_B = prob(B, Y, spam=True)
+                p_C = prob(C, Y, spam=True)
+                # Calculate entropy for child nodes
+                e_B = entropy([p_B, 1.0-p_B])
+                e_C = entropy([p_C, 1.0-p_C])
+                # Calculate scaling factors for child nodes entropy
+                k_B = float(B.count(True))/A_count
+                k_C = float(C.count(True))/A_count
+                # Calculate scaled entropy sum for child nodes
+                e_S = k_B*e_B + k_C*e_C
+                # Calculate information gain for the split
+                IG_curr = e_A - e_S
+                #print IG_curr
+                if IG_curr < IG_prev:
+                    break
+                else:
+                    IG_prev = IG_curr
+                IG_list.append((f,t,IG_curr))
+            i += 1
+            # Check end condition
+            if i >= len(pairs)-1:
+                break
+
+    max_val = max(IG[2] for IG in IG_list)
+    print "Min:",[IG for IG in IG_list if IG[2] == max_val]
+
+A = data
+
+#build_tree1(fmat, feature_list, thresh)
+#build_tree2(fmat, feature_list)
+eval_node(A, fmat, feature_list)

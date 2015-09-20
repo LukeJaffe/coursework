bf6ee28ff62f88e10d570256c481a2343cd701f6
Luke Jaffe <lukejaffe1@gmail.com>
2015-09-19 20:17:37 -0400

    Add working solution to hw1 p2 (spam only)

---
+++ fall_2015/machine_learning/hw1/p2/solver.py
@@ -0,0 +1,129 @@
+import numpy as np
+
+# Load data
+def get_features(fname):
+    feature_list = []
+    f = open(fname, "r")
+    for line in f:
+        if line.find('|') == -1:
+            if line.strip():
+                line = line.split(':')
+                feature = line[0]
+                desc = line[1] 
+                desc = desc.replace(" ","")
+                desc = desc.split('.')[0]
+                feature_list.append(feature)
+    return feature_list
+
+# Create dictionary with features as keys
+def get_data(fname, feature_list):
+    dmat = []
+    fmat = {}
+    for feature in feature_list:
+        fmat[feature] = []
+
+    Y = []
+    f = open(fname, "r")
+    for line in f:
+        x = line.split(',')
+        Y.append(float(x[-1].strip()))
+        x = [float(e) for e in x]
+        dmat.append(x)
+        for i in range(len(x)-1):
+            x[i] = float(x[i])
+            fmat[feature_list[i]].append(x[i])
+    return fmat,np.array(dmat),np.array(Y)
+
+
+class KFolder:
+    def __init__(self, X, k):
+        self.k = k
+        self.X = []
+        np.random.shuffle(X)
+        tot_len = len(X)
+        fold_len = tot_len/k
+        for i in range(0, tot_len, fold_len):
+            self.X.append(X[i:i+fold_len])
+
+        # Prepare the data dictionary
+        training = {"data":[], "labels":[]}
+        testing = {"data":[], "labels":[]}
+        self.data = {"training":training,"testing":testing}
+
+        # Prepare training and testing data
+        for i in range(self.k):
+            self.data["testing"]["data"].append(self.X[i].T[:-1])
+            self.data["testing"]["labels"].append(self.X[i].T[-1:])
+            #print self.data["testing"]["data"][i].shape,self.data["testing"]["labels"][i].shape
+            tmp = self.X[:i]+self.X[i+1:]
+            tmp = np.concatenate(tmp, axis=0)
+            d = tmp.T[:-1]
+            l = tmp.T[-1:]
+            self.data["training"]["data"].append(d)
+            self.data["training"]["labels"].append(l)
+            #print self.data["training"]["data"][i].shape,self.data["training"]["labels"][i].shape
+            #print
+
+
+        # Normalize the data
+        for i in range(self.k):
+            d = self.data["testing"]["data"][i]
+            t = self.data["training"]["data"][i]
+            for j in range(len(d)):
+                # Normalize the training data
+                min_val = d[j].min()
+                d[j] -= min_val
+                max_val = d[j].max()
+                d[j] /= max_val
+                # Normalize the testing data with the same parameters
+                t[j] -= min_val
+                t[j] /= max_val
+                # Clip testing data to [0.0, 1.0] 
+                t[j] = np.clip(t[j], 0.0, 1.0)
+            #print self.data["testing"]["data"][i].min(), self.data["testing"]["data"][i].max()
+            #print self.data["training"]["data"][i].min(), self.data["training"]["data"][i].max()
+            #print 
+
+    def training(self, k):
+        return self.data["training"]["data"][k].T, self.data["training"]["labels"][k].T
+
+    def testing(self, k):
+        return self.data["testing"]["data"][k].T, self.data["testing"]["labels"][k].T
+
+    
+
+if __name__=="__main__":
+    # Get feature list
+    fname1 = ("/home/jaffe5/Documents/classes/fall_2015/"+
+                "machine_learning/hw1/data/spambase/spambase.names")
+    feature_list = get_features(fname1)
+
+    # Get data
+    fname2 = ("/home/jaffe5/Documents/classes/fall_2015/"
+                +"machine_learning/hw1/data/spambase/spambase.data")
+    fmat,X,Y = get_data(fname2, feature_list)
+
+    k = 10
+    kfolder = KFolder(X,k)
+    for i in range(k):
+        # Get data and labels at fold k
+        Xi,Yi = kfolder.training(i)
+
+        # Solve for the vector of linear factors, W
+        p1 = np.dot(Xi.T,Xi)
+        i1 = np.linalg.pinv(p1)
+        p2 = np.dot(Xi.T, Yi)
+        W = np.dot(i1, p2)
+        print W.shape
+
+        # Test solution against the testing data
+        Xti,Yti = kfolder.testing(i)
+        total = len(Yti)
+        correct = 0
+        for j in range(len(Xti)):
+            s = 0.0
+            for k in range(len(Xti[j])):
+                s += Xti[j][k]*W[k]
+            if round(s) == Yti[j]:
+                correct += 1
+        print "Correct:",str(correct)+str('/')+str(total)+' =',float(correct)/float(total)

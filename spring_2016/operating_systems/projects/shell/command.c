0fe8995d0bcd5c679a5332554882154bb3d696a0
Luke Jaffe <lukejaffe1@gmail.com>
2016-03-03 20:39:23 -0500

    finish sub-command piping, start i/o redirection

--- spring_2016/operating_systems/projects/shell/command.c
+++ spring_2016/operating_systems/projects/shell/command.c
@@ -215,92 +215,47 @@ void ReadRedirectAndBackground(struct Command* command)
         argv[end_index] = NULL;
 }
 
-/* Fork a child process to execute the ls command
- * with its output redirected into a pipe */
-void RunSubCommand(int* fds1, int* fds2, char** argv, bool input, bool output)
+/* Run a subcommand and handle piping */
+void RunSubCommand(int** fds_list, int idx1, int idx2, int num_pipes, char** argv)
 {
-    printf("Run sub command\n");
-    // Command supplies input to a pipe and takes output from a pipe
-    if (input == true && output == true)
+    int ret = fork();
+    if (ret < 0)
     {
-        int ret = fork();
-        if (ret < 0)
-        {
-            perror("fork");
-            exit(1);
-        }
-        else if (ret == 0)
-        {
-            // input of command is output of pipe1
-            close(fds1[1]);
-            close(0);
-            dup(fds1[0]);
-            close(fds1[0]);
-            // output of command is input of pipe2
-            close(fds2[0]);
-            close(1);
-            dup(fds2[1]);
-            close(fds2[1]);
-        }
-        else return;
+        perror("fork");
+        exit(1);
     }
-    // Command supplies input to a pipe
-    if (input == true && output == false)
+    else if (ret == 0)
     {
-        int ret = fork();
-        if (ret < 0)
+        int i;
+        for (i = 0; i < num_pipes; i++)
         {
-            perror("fork");
-            exit(1);
+            if (i == idx1)
+            {
+                close(fds_list[i][1]);
+                close(0);
+                dup(fds_list[i][0]);
+            }
+            else if (i == idx2)
+            {
+                close(fds_list[i][0]);
+                close(1);
+                dup(fds_list[i][1]);
+            }
+            else
+            {
+                close(fds_list[i][0]);
+                close(fds_list[i][1]);
+            }
         }
-        else if (ret == 0)
-        {
-            close(fds2[0]);
-            close(1);
-            dup(fds2[1]);
-            close(fds2[1]);
-        }
-        else return;
-    }
-    // Command takes output from a pipe
-    else if (input == false && output == true)
-    {
-        int ret = fork();
-        if (ret < 0)
-        {
-            perror("fork");
-            exit(1);
-        }
-        else if (ret == 0)
-        {
-            close(fds1[1]);
-            close(0);
-            dup(fds1[0]);
-            close(fds1[0]);
-        }
-        else return;
-    }
-    // Command does not use a pipe
-    else if (input == false && output == false)
-    {
-        printf("No pipe\n");
-        int ret = fork();
-        if (ret < 0)
-        {
-            perror("fork");
-            exit(1);
-        }
-        else if (ret == 0)
-        {
-            // Nothing
-        }
-        else return;
-    }
 
-    printf("Finish sub command\n");
-    if (execvp(argv[0], argv) == -1)
+        if (execvp(argv[0], argv) == -1)
+        {
+            printf("%s: Command not found\n", argv[0]);
+        }
+    }
+    else
     {
-        printf("%s: Command not found\n", argv[0]);
+        // nothing
     }
 }
 
@@ -310,7 +265,7 @@ void RunCommand(struct Command* command)
     int i;
     if (command->num_sub_commands == 0)
     {
-        printf("Empty command.\n");
+        //printf("Empty command.\n");
         return;
     }
 
@@ -321,18 +276,10 @@ void RunCommand(struct Command* command)
         return;
     }
 
-    // Check redirection, background
+    
+
+    // background
     /*
-    if (command->stdin_redirect == NULL)
-        printf("\nRedirect stdin: NULL\n");
-    else
-        printf("\nRedirect stdin: %s\n", command->stdin_redirect);
-
-    if (command->stdout_redirect == NULL)
-        printf("Redirect stdout: NULL\n");
-    else
-        printf("Redirect stdout: %s\n", command->stdout_redirect);
-
     if (command->background)
         printf("Background: yes\n");
     else
@@ -352,23 +299,51 @@ void RunCommand(struct Command* command)
         }
     }
 
+    // i/o redirection
+    int read_fd, write_fd; 
+
+    if (command->stdin_redirect != NULL)
+    {
+        printf("\nRedirect stdin: %s\n", command->stdin_redirect);
+        read_fd = open(command->stdin_redirect);
+        if (read_fd != -1)
+        {
+            fds_list[0][0] = read_fd;
+        }
+    }
+    else
+        printf("\nRedirect stdin: NULL\n");
+
+    if (command->stdout_redirect != NULL)
+    {
+        printf("Redirect stdout: %s\n", command->stdout_redirect);
+        write_fd = creat(command->stdout_redirect, 0660);
+        if (write_fd != -1)
+        {
+            fds_list[0][1] = write_fd;
+        }
+    }
+    else
+        printf("Redirect stdout: NULL\n");
+
+    // execute sub-commands
     if (command->num_sub_commands == 1)
     {
-        printf("\nCommand without piping.\n");
-        RunSubCommand(NULL, NULL, command->sub_commands[0].argv, false, false);
+        //printf("\nCommand without piping.\n");
+        RunSubCommand(fds_list, -1, -1, num_pipes, command->sub_commands[0].argv);
     }
     else
     {
-        printf("\nCommand has # pipes: %d\n", num_pipes);
-        RunSubCommand(NULL, fds_list[0], command->sub_commands[0].argv, true, false);
+        //printf("\nCommand has # pipes: %d\n", num_pipes);
+        RunSubCommand(fds_list, -1, 0, num_pipes, command->sub_commands[0].argv);
         for (i = 0; i < num_pipes-1; i++)
         {
-            RunSubCommand(fds_list[i], fds_list[i+1], command->sub_commands[i+1].argv, true, true);
+            RunSubCommand(fds_list, i, i+1, num_pipes, command->sub_commands[i+1].argv);
         }
-        RunSubCommand(fds_list[num_pipes-1], NULL, command->sub_commands[num_pipes].argv, false, true);
+        RunSubCommand(fds_list, num_pipes-1, -1, num_pipes, command->sub_commands[num_pipes].argv);
     }
 
-    /* Parent must close these fds to avoid issues */
+    /* Parent must close all fds to avoid issues */
     for (i = 0; i < num_pipes; i++)
     {
         close(fds_list[i][0]);
@@ -381,7 +356,7 @@ void RunCommand(struct Command* command)
         int ret = wait(NULL);
         if (ret == -1)
             break;
-        else
-            printf("Process %d finished\n", ret);
+        //else
+        //    printf("Process %d finished\n", ret);
     }
 }

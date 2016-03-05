546a8f95b64b315eb8d8929df78b9a2c026eba38
Luke Jaffe <lukejaffe1@gmail.com>
2016-03-04 22:42:24 -0500

    Final commit pre-submission

--- spring_2016/operating_systems/projects/shell/stable/main.c
+++ spring_2016/operating_systems/projects/shell/stable/main.c
@@ -1,49 +1,24 @@
+#include <string.h>
+#include <stdio.h>
+
 #include "command.h"
-#include "terminal.h"
+
+#define MAX_LINE_LEN        (200)
 
 int main()
 {
-    static struct termios oldt, newt;
-
-    /* get parameters of the current terminal
-    STDIN_FILENO will tell tcgetattr that it should write the settings
-    of stdin to oldt */
-    tcgetattr( STDIN_FILENO, &oldt);
-    
-    /* now the settings will be copied */
-    newt = oldt;
-
-    /* ICANON normally takes care that one line at a time will be processed
-    that means it will return if it sees a "\n" or an EOF or an EOL */
-    newt.c_lflag &= ~(ICANON | ECHO);          
-
-    /* Those new settings will be set to STDIN
-    TCSANOW tells tcsetattr to change attributes immediately. */
-    tcsetattr( STDIN_FILENO, TCSANOW, &newt);
-
-    char history[MAX_HISTORY_SIZE][MAX_LINE_LEN];
-    int history_len = 0;
-
     /* Infinite loop for input, exits on ctrl+c */
     while (1)
     {
+        char line[MAX_LINE_LEN];
         /* Print prompt */
         printf("$ ");
-        fflush(stdout);
 
-        /* Get line from user */
-        char line[MAX_LINE_LEN];
-        memset(line, 0, MAX_LINE_LEN);
-        GetLine(line, history, history_len, history_len);
+        /* Read a string from the user */
+        fgets(line, sizeof line, stdin);
 
-        /* Add this line to the command history */
-        if (history_len < MAX_HISTORY_SIZE && 
-                strlen(line) > 0 &&
-                strcmp(line, history[history_len-1]) != 0)
-        {
-            strcpy(history[history_len], line);
-            history_len++;
-        }
+        /* Remove newline character from input */
+        line[strlen(line)-1] = '\0';
 
         /* Parse string into subcommands and special chars */
         struct Command command; 
@@ -56,9 +31,6 @@ int main()
         else
             RunCommand(&command, -1);
     }
-
-    /* Restore the old settings */
-    tcsetattr( STDIN_FILENO, TCSANOW, &oldt);
 
     return 0;
 }
